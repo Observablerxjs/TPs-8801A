efec02b3763b74fd997473fece6d387fe6e28be1
Yacine Benouniche <yacine.benouniche@polymtl.ca>
2018-11-10 12:19:53 -0500

    TP4

--- TP3/Bilateral/matlab/main.m
+++ TP3/Bilateral/matlab/main.m
@@ -9,13 +9,13 @@ close all
 
 noise = rgb2hsv(imread('../data/taj-rgb-noise.jpg'));
 figure;
-%imshow(noise(:,:,3)); title('Image originale (bruit�e)');
+imshow(noise(:,:,3)); title('Image originale (bruit�e)');
 
 
 % TODO Question 1 :
 filtered = bilateralFilter(noise(:,:,3),[],0,1,min(size(noise,1),size(noise,2))/16,1/4);
 figure;
-%imshow(filtered); title('Image filtr�e');
+imshow(filtered); title('Image filtr�e');
 
 %% Tone mapping
 % Il s'agit de compresser la plage d'intensit�es d'une image en pr�servant
@@ -25,35 +25,41 @@ figure;
 % lecture de l'image hdr (� partir de 3 expositions diff�rentes)
 srcFolder = '../data/hdr/memorial/'; ext = '.png';
 src = double(imread([srcFolder 'low' ext])) + double(imread([srcFolder 'mid' ext])) + double(imread([srcFolder 'high' ext]));
-figure; imshow(src); title('Avant normalization')
+
+
 % normalisation
-ReductionULineaire = src - min(src(:));
-ReductionULineaire = ReductionULineaire./max(ReductionULineaire(:));
-figure; imshow(ReductionULineaire); title('Réduction uniforme linéaire')
+src = src - min(src(:));
+src = src./max(src(:));
+
+figure; imshow(src); title('Avec Reduction uniforme Lineaire')
+
+src = rgb2hsv(src);
 
 % Filtrage avec filtres Gaussien et bilatéral (Question 2)
-filteredGauss(:,:,1) = imgaussfilt(src(:,:,1),250);
-filteredGauss(:,:,2) = imgaussfilt(src(:,:,2),250);
-filteredGauss(:,:,3) = imgaussfilt(src(:,:,3),250);
 
-disp(max(filteredGauss(:)));
-disp(min(filteredGauss(:)));
+filteredGauss(:,:,3) = imgaussfilt(src(:,:,3),6);
+filteredGauss(:,:,3) = src(:,:,3) - filteredGauss(:,:,3);
+filteredGauss = filteredGauss-min(filteredGauss(:));
+filteredGauss = filteredGauss./max(filteredGauss(:));
+filteredGauss(:,:,1) = src(:,:,1);
+filteredGauss(:,:,2) = src(:,:,2);
+filteredGauss = hsv2rgb(filteredGauss);
+
+figure; imshow(filteredGauss); title('Avec passe-haut Gaussien');
 
 
+filteredBillateral(:,:,3) = bilateralFilter(src(:,:,3),[],min(min(src(:,:,3))),max(max(src(:,:,3))),25,1/4);
+filteredBillateral(:,:,3) = src(:,:,3) - filteredBillateral(:,:,3);
+filteredBillateral = filteredBillateral-min(filteredBillateral(:));
+filteredBillateral = filteredBillateral./max(filteredBillateral(:));
 
-figure; imshow(filteredGauss); title('Image apres filtre gaussien')
+filteredBillateral(:,:,1) = src(:,:,1);
+filteredBillateral(:,:,2) = src(:,:,2);
+filteredBillateral = hsv2rgb(filteredBillateral);
 
-diffGauss = src - filteredGauss;
-figure; imshow(diffGauss); title('DiffGauss')
+figure; imshow(filteredBillateral); title('Avec passe-haut Bilateral')
 
-filteredBillateral(:,:,1) = bilateralFilter(src(:,:,1),[],min(src(:)),max(src(:)),min(size(src,1),size(src,2))/16,1/4);
-filteredBillateral(:,:,2) = bilateralFilter(src(:,:,2),[],min(src(:)),max(src(:)),min(size(src,1),size(src,2))/16,1/4);
-filteredBillateral(:,:,3) = bilateralFilter(src(:,:,3),[],min(src(:)),max(src(:)),min(size(src,1),size(src,2))/16,1/4);
 
-%figure; imshow(filteredBillateral); title('Image apres filtre bilateral')
-
-diffBilateral = src - filteredBillateral;
-%figure; imshow(diffBilateral); title('DiffBilateral')
 
 
 
