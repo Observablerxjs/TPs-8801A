2f89be7fbb81968449d14fa110c758b481f36a1d
Léandre Chamberland-Dozois <lchamberland-dozois@hotmail.com>
2018-11-17 12:04:02 -0500

    structure added

---
+++ Projet/Bootstrap.py
@@ -0,0 +1,18 @@
+class Bootstrap:
+    def __init__():
+        self.iimProc =
+        self.fingDetec =
+        self.edgeDetec = 
+        self.clip =
+        self.classifier = 
+        self.boundTrac =
+
+    def run(args):
+        if
+
+        else:
+            processedImage = 
+            edges = self.edgeDetec
+            clippedEdges = self.clip
+            
+
