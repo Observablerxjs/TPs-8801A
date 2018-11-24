390842beaf4ae6a8a4bf236c9a352bd160d16503
Léandre Chamberland-Dozois <lchamberland-dozois@hotmail.com>
2018-11-24 15:26:37 -0500

    descriptor added

--- Projet/Classifier/DescriptorGenerator.py
+++ Projet/Classifier/DescriptorGenerator.py
@@ -1,9 +1,34 @@
+import numpy as np
+
+from scipy.interpolate import interp1d
+
 class DescriptorGenerator:
+    nb_points = 200
+    desc_size = 20
 
     @staticmethod
     def generate_origin_descriptor():
-        return 0
+        return [0] * (DescriptorGenerator.nb_points + 1)
 
     @staticmethod
     def generate_descriptor(data):
-        return 0
+        nb_fingertips = data[1]
+        array_index = data[2]
+
+        xs = range(0, len(array_index))
+
+        f = interp1d(xs, array_index)
+
+        new_xs = np.linspace(0, len(xs), DescriptorGenerator.nb_points)
+        new_ys = []
+
+        for i in range(0, len(new_xs)):
+            new_ys.append(f(new_xs[i]))
+
+        new_ys = np.absolute(np.array(new_ys)).tolist()
+        new_ys = new_ys[-2]
+        new_ys = (np.array(new_ys) / new_ys[0]).tolist()
+        new_ys = new_ys[:DescriptorGenerator.desc_size]
+        new_ys.append(nb_fingertips)
+
+        return new_ys
